6d1a495d125c4fdabe18819a2dc220b805f76ee3
Olga Zaiats <olga_zaiats@ua.fm>
2017-05-06 09:36:53 +0300

    Lesson 4. Homework.

---
+++ src/main/java/com/kitcenter/app/utils/convertors/PrimitiveConvertor.java
@@ -0,0 +1,31 @@
+package com.kitcenter.app.utils.convertors;
+
+public class PrimitiveConvertor {
+    float float1 = 1.25f;
+    char char1;
+    int int2 = 15;
+    char char2;
+    char char3 = 'c';
+    int int3;
+
+    public char floatToChar(){
+        char1 = (char) float1;
+        System.out.println("Input float value is " + float1 + ". Output char value is " + char1);
+        return char1;
+    }
+
+    public char intToChar(){
+        char2 = (char) int2;
+        System.out.println("Input int value is " + int2 + ". Output char value is " + char2);
+        return char2;
+    }
+
+    public int charToInt(){
+        int3 = (int) char3;
+        System.out.println("Input char value is " + char3 + ". Output int value is " + int3);
+        return int3;
+    }
+
+
+
+}
---
+++ src/main/java/com/kitcenter/app/utils/runners/Lesson3Runner.java
@@ -0,0 +1,13 @@
+package com.kitcenter.app.utils.runners;
+
+import com.kitcenter.app.utils.convertors.PrimitiveConvertor;
+
+public class Lesson3Runner {
+    public static void main(String args[]) {
+        PrimitiveConvertor primitiveConvertor = new PrimitiveConvertor();
+        primitiveConvertor.floatToChar();
+        primitiveConvertor.intToChar();
+        primitiveConvertor.charToInt();
+
+    }
+}

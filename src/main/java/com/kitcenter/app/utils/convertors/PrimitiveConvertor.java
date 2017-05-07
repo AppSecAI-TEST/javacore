7094b975aac9f71fcf340b281d318d4731c56418
Olga Zaiats <olga_zaiats@ua.fm>
2017-05-07 11:05:25 +0300

    Lesson 3. Homework optimized.

--- src/main/java/com/kitcenter/app/utils/convertors/PrimitiveConvertor.java
+++ src/main/java/com/kitcenter/app/utils/convertors/PrimitiveConvertor.java
@@ -3,10 +3,8 @@ package com.kitcenter.app.utils.convertors;
 public class PrimitiveConvertor {
     float float1 = 97.15f;
     char char1;
-    int int2 = 98;
+    int int1 = 98;
     char char2;
-    char char3 = 'c';
-    int int3;
 
     public char floatToChar(){
         char1 = (char) float1;
@@ -14,18 +12,13 @@ public class PrimitiveConvertor {
         return char1;
     }
 
-    public char intToChar(){
-        char2 = (char) int2;
-        System.out.println("Input int value is " + int2 + ". Output char value is " + char2);
-        return char2;
+    public void intToChar(){
+        char2 = (char) int1;
+        System.out.println("Input int value is " + int1 + ". Output char value is " + char2);
     }
 
-    public int charToInt(){
-        int3 = (int) char3;
-        System.out.println("Input char value is " + char3 + ". Output int value is " + int3);
-        return int3;
+    public void charToInt(){
+        int1 = (int) char1;
+        System.out.println("Input char value is " + char1 + ". Output int value is " + int1);
     }
-
-
-
 }
